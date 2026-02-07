91594af0da76846a9ed858cea3095707f18b93f6
dlgkrals <dlgkrals1136@naver.com>
2026-02-08 03:47:35 +0900

    fix: 베이스 url 백엔드 url로 변경

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://api.ictwiki.site';
 
 // Axios instance 생성
 const apiClient = axios.create({
